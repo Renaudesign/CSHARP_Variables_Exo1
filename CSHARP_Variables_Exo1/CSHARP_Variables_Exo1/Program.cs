501994c4800b2d81404da87b0f05b5c5c58d02d6
renaudesign <renaud.sourmail@gmail.com>
2021-02-15 16:52:10 +0100

    bases de fichiers

---
+++ CSHARP_Variables_Exo1/CSHARP_Variables_Exo1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSHARP_Variables_Exo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Consonne = 1;
+            Voyelle = 2;
+        }
+    }
+}
