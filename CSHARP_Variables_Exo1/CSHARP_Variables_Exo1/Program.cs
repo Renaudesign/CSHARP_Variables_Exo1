b7b8bb89773460b613cf83eb7bacac90cd9cd142
renaudesign <renaud.sourmail@gmail.com>
2021-02-16 09:44:32 +0100

    anglicisation

--- CSHARP_Variables_Exo1/CSHARP_Variables_Exo1/Program.cs
+++ CSHARP_Variables_Exo1/CSHARP_Variables_Exo1/Program.cs
@@ -6,14 +6,14 @@ namespace CSHARP_Variables_Exo1
     {
         static void Main(string[] args)
         {
-            int valeur1 = 14;
-            int valeur2 = 66;
-            int valeur3 = valeur1 + valeur2;
+            int value1 = 14;
+            int value2 = 66;
+            int somme = value1 + value2;
 
-            Console.WriteLine("Je prends d'abord le nombre " + valeur1);
-            Console.WriteLine("je lui projette violemment le nombre " + valeur2);
-            Console.WriteLine("Et PAF ça donne " + valeur3);
-            Console.WriteLine("Donc si je réssume tout ce fatras d'informations. " + valeur1 + " et " + valeur2 + " additionés font " + valeur3 + ".");
+            Console.WriteLine("Je prends d'abord le nombre " + value1);
+            Console.WriteLine("je lui projette violemment le nombre " + value2);
+            Console.WriteLine("Et PAF ça donne " + somme);
+            Console.WriteLine("Donc si je réssume tout ce fatras d'informations. " + value1 + " et " + value2 + " additionés font " + somme + ".");
         }
     }
 }
