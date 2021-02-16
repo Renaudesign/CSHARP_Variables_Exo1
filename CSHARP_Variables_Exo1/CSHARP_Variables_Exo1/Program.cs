75d6e1415bea83de720073bbc62d13cee57fead1
renaudesign <renaud.sourmail@gmail.com>
2021-02-16 09:35:07 +0100

    corrections de la V1

--- CSHARP_Variables_Exo1/CSHARP_Variables_Exo1/Program.cs
+++ CSHARP_Variables_Exo1/CSHARP_Variables_Exo1/Program.cs
@@ -6,8 +6,10 @@ namespace CSHARP_Variables_Exo1
     {
         static void Main(string[] args)
         {
-            Consonne = 1;
-            Voyelle = 2;
+            Console.WriteLine("Je prends d'abord le nombre " + valeur1);
+            Console.WriteLine("je lui projette violemment le nombre " valeur2);
+            Console.WriteLine("Et PAF ça donne "valeur3);
+            Console.WriteLine("Donc si je réssume tout ce fatras d'informations. " + valeur1 + " et " + valeur2 + " additionés font " + valeur3 + ".");
         }
     }
 }
