bc23f8b3a87a421b02639b3513f8108589800d0a
renaudesign <renaud.sourmail@gmail.com>
2021-02-16 09:23:04 +0100

    variante de concatenation

--- CSHARP_Variables_Exo1/CSHARP_Variables_Exo1/Program.cs
+++ CSHARP_Variables_Exo1/CSHARP_Variables_Exo1/Program.cs
@@ -6,8 +6,13 @@ namespace CSHARP_Variables_Exo1
     {
         static void Main(string[] args)
         {
-            Consonne = 1;
-            Voyelle = 2;
+            int valeur1 = 14;
+            int valeur2 = 66;
+            int valeur3 = valeur1 + valeur2;
+            Console.WriteLine ($"Je prends d'abord le nombre {valeur1}");
+            Console.WriteLine ($"je lui projette violemment le nombre {valeur2}");
+            Console.WriteLine ($"Et PAF ça donne {valeur3}");
+            Console.WriteLine($"Donc si je réssume tout ce fatras d'informations. {valeur1} et {valeur2} additionés font {valeur3}");
         }
     }
 }
